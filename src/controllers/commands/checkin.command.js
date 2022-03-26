c03c19adb604f71fbee9456b2c133d0796bfdf6f
Tanmay Naik <13809245+tsnaik@users.noreply.github.com>
2022-03-26 13:22:05 -0700

    WIP helth today command

--- src/controllers/commands/checkin.command.js
+++ src/controllers/commands/checkin.command.js
@@ -1,2 +1,34 @@
-const SAMPLE_MESSAGE = "Dummy response to a checkin \\.";
-export default (ctx) => ctx.reply(SAMPLE_MESSAGE, { parse_mode: "MarkdownV2" });
+import chrono from 'chrono-node'
+
+const allowList = ['today', 'yesterday']
+
+const processSubCommand = (subCommand) => {
+    if (!allowList.includes(subCommand)) {
+        throw new Error(`Invalid value after the command: ${subCommand}`)
+    }
+    const date = chrono.parseDate(subCommand)
+    console.log(`logged date: ${date}`)
+    // TODO make some calls to DB to add entry.
+    return true
+}
+
+export default (ctx) => {
+  console.log(ctx.message)
+  const entity = ctx.message.entities.find((value) => value.type === 'bot_command');
+  const appendedText = ctx.message.text.substring(entity.length - entity.offset + 1);
+  console.log(appendedText)
+
+  if (!appendedText) {
+      ctx.reply(`Please provide a time after ${ctx.message.text}. Possible options are: ${allowList.join(', ')}. \n\nExample: Send "/helth today" to record today's activity.`)
+      return
+  }
+
+  try {
+      const isOk = processSubCommand(appendedText)
+      if (isOk) {
+          ctx.reply(`${ctx.message.from.first_name}, I have recorded your entry for ${appendedText}. Kudos for getting it done! 🎉`)
+      }
+  } catch (error) {
+      ctx.reply(error.message)
+  }
+};
--- src/controllers/commands/commandMap.js
+++ src/controllers/commands/commandMap.js
@@ -3,5 +3,5 @@ import checkin from "./checkin.command.js";
 
 export default {
   help,
-  helthtoday: checkin, // key of the export is the command name: ie the user will call /helthtoday
+  helth: checkin, // key of the export is the command name: ie the user will call /helth
 };
