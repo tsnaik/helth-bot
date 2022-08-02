fe7e8793b1b87b79c0512f0eb0552f64644109d4
Aniket Jingar <aniketjingar@gmail.com>
2022-08-02 02:27:43 +0000

    Move helth-bot-app code to root folder

--- helth-bot-app/src/controllers/commands/commandMap.js
+++
@@ -1,7 +0,0 @@
-import help from "./help.command.js";
-import checkin from "./checkin.command.js";
-
-export default {
-  help,
-  helthtoday: checkin, // key of the export is the command name: ie the user will call /helthtoday
-};
