8b3ad11f34b119dd8a72842b996423b06da36b9a
Aniket Jingar <aniketjingar@gmail.com>
2022-08-02 02:27:43 +0000

    Move helth-bot-app code to root folder

--- helth-bot-app/src/controllers/commands/checkin.command.js
+++
@@ -1,2 +0,0 @@
-const SAMPLE_MESSAGE = "Dummy response to a checkin \\.";
-export default (ctx) => ctx.reply(SAMPLE_MESSAGE, { parse_mode: "MarkdownV2" });
--- helth-bot-app/src/controllers/commands/help.command.js
+++
@@ -1,2 +0,0 @@
-const HELP_MESSAGE = "help: dummy help string\\.";
-export default (ctx) => ctx.reply(HELP_MESSAGE, { parse_mode: "MarkdownV2" });
